0f586aae1f0541a67a45f0f440bb7f6b208855b4
abdelrahmanmady <adomady55@outlook.com>
2025-11-25 19:40:17 +0200

    CategoryCreateDto_Added

---
+++ MyApp.API/DTOs/CategoryCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyApp.API.DTOs
+{
+    public class CategoryCreateDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
