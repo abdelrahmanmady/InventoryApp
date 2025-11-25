cbf38bff1e69bc9f18f9e7e71555c770bc6c18db
abdelrahmanmady <adomady55@outlook.com>
2025-11-25 18:37:21 +0200

    FinishedRepoPattern

---
+++ MyApp.Data/Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Data.Data;
+using MyApp.Data.Entities;
+using MyApp.Data.Repositories.Interfaces;
+
+namespace MyApp.Data.Repositories.Implementations
+{
+    public class ProductRepository(AppDbContext context) : IProductRepository
+    {
+        private readonly AppDbContext _context = context;
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+
+        }
+        public async Task<bool> CreateAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return false;
+            _context.Products.Remove(product);
+            return await _context.SaveChangesAsync() > 0;
+
+        }
+    }
+}
--- MyApp.Data/Repositories/Interfaces/ICategoryRepository.cs
+++ MyApp.Data/Repositories/Interfaces/ICategoryRepository.cs
@@ -5,7 +5,7 @@ namespace MyApp.Data.Repositories.Interfaces
     public interface ICategoryRepository
     {
         Task<IEnumerable<Category>> GetAllAsync();
-        Task<Category> GetByIdAsync(int id);
+        Task<Category?> GetByIdAsync(int id);
         Task<bool> CreateAsync(Category category);
         Task<bool> UpdateAsync(Category category);
         Task<bool> DeleteAsync(int id);
